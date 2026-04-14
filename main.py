fae5450f64e0a33e09a32f54553e9869d0093630
antoinerougier <antoinerougier77@gmail.com>
2026-04-14 14:58:28 +0200

    Feat: add the main.py where the model is train and i evaluate the result

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+from src.preprocessing import preprocess_train
+from src.model import train_model, save_model
+from src.evaluation import evaluate_model, print_results
+
+
 def main():
-    print("Hello from house-pricing!")
+    train_csv = "data/train.csv"  # Change selon ton chemin
+
+    print("Prétraitement...")
+    X_train, X_test, y_train, y_test = preprocess_train(
+        train_csv, scaler_path="scaler.joblib"
+    )
+
+    print("Entraînement du modèle (GridSearch)...")
+    model = train_model(X_train, y_train)
+
+    print("Évaluation...")
+    results = evaluate_model(model, X_test, y_test)
+    print_results(results)
+
+    save_model(model, path="model.joblib")
 
 
 if __name__ == "__main__":
